36cc563136375c200f83e4a69e63561fa1186d51
mifune96 <imrank.sensea@gmail.com>
2021-06-01 12:02:20 +0700

    first commit

---
+++ app/src/main/java/tomuch/coffee/tokoonline/MainActivity.kt
@@ -0,0 +1,11 @@
+package tomuch.coffee.tokoonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
