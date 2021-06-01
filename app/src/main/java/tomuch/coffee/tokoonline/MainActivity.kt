74ac431ed9411844bf3a3763abbbd5db0e997a8b
mifune96 <imrank.sensea@gmail.com>
2021-06-01 14:29:13 +0700

    any progress

--- app/src/main/java/tomuch/coffee/tokoonline/MainActivity.kt
+++ app/src/main/java/tomuch/coffee/tokoonline/MainActivity.kt
@@ -2,10 +2,71 @@ package tomuch.coffee.tokoonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.fragment_home.*
+import tomuch.coffee.tokoonline.fragment.AkunFragment
+import tomuch.coffee.tokoonline.fragment.HomeFragment
+import tomuch.coffee.tokoonline.fragment.KeranjangFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentHome : Fragment = HomeFragment()
+    private val fragmentAkun : Fragment = AkunFragment()
+    private val fragmentKeranjang : Fragment = KeranjangFragment()
+    private val fm: FragmentManager = supportFragmentManager
+    private var active: Fragment = fragmentHome
+
+    private lateinit var menu: Menu
+    private lateinit var menuItem: MenuItem
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpBottomNav()
+    }
+
+    fun setUpBottomNav (){
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+        menu = bottomNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
+
+        bottomNavigationView.setOnNavigationItemReselectedListener { item ->
+            when(item.itemId){
+                R.id.navigation_home -> {
+                    callFragment(0,fragmentHome)
+                }
+
+                R.id.navigation_keranjang -> {
+                    callFragment(1,fragmentKeranjang)
+                }
+
+                R.id.navigation_akun -> {
+                    callFragment(2,fragmentAkun)
+                }
+
+            }
+
+            false
+        }
+    }
+
+    fun callFragment(int: Int,fragment: Fragment){
+        Log.d("Respons", "Akun ")
+        menuItem = menu.getItem(int)
+        menuItem.isChecked = true
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragment
     }
 }
---
+++ app/src/main/java/tomuch/coffee/tokoonline/fragment/KeranjangFragment.kt
@@ -0,0 +1,31 @@
+package tomuch.coffee.tokoonline.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import tomuch.coffee.tokoonline.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class KeranjangFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_keranjang, container, false)
+    }
+
+
+}
