93239696b4e586984030fc63c895ba829cc18131
mifune96 <imrank.sensea@gmail.com>
2021-06-02 16:02:57 +0700

    sharepreferance

--- app/src/main/java/tomuch/coffee/tokoonline/activity/LoginActivity.kt
+++ app/src/main/java/tomuch/coffee/tokoonline/activity/LoginActivity.kt
@@ -2,11 +2,20 @@ package tomuch.coffee.tokoonline.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 import tomuch.coffee.tokoonline.R
+import tomuch.coffee.tokoonline.helper.SharedPref
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var s:SharedPref
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        s = SharedPref(this)
+        btn_prosesLogin.setOnClickListener {
+            s.setStatusLogin(true)
+        }
     }
 }
--- app/src/main/java/tomuch/coffee/tokoonline/fragment/AkunFragment.kt
+++ app/src/main/java/tomuch/coffee/tokoonline/fragment/AkunFragment.kt
@@ -1,11 +1,14 @@
 package tomuch.coffee.tokoonline.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import tomuch.coffee.tokoonline.R
+import tomuch.coffee.tokoonline.helper.SharedPref
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,13 +21,25 @@ import tomuch.coffee.tokoonline.R
  */
 class AkunFragment : Fragment() {
 
+    lateinit var s:SharedPref
+    lateinit var btnLogout:Button
 
+    @SuppressLint("UseRequireInsteadOfGet")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_akun, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_akun, container, false)
+        btnLogout = view.findViewById(R.id.btn_logout)
+
+        s = SharedPref(activity!!)
+
+        btnLogout.setOnClickListener {
+            s.setStatusLogin(false)
+        }
+
+        return view
     }
 
 
