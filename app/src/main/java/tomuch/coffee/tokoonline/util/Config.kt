6f2b0fe2b67943c53c518d53a288e6dacf0f0ee8
mifune96 <imrank.sensea@gmail.com>
2021-09-10 05:48:34 +0700

    add new icon and etc

--- app/src/main/java/tomuch/coffee/tokoonline/util/Config.kt
+++ app/src/main/java/tomuch/coffee/tokoonline/util/Config.kt
@@ -1,7 +1,7 @@
 package tomuch.coffee.tokoonline.util
 
 object Config {
-    const val baseUrl = "http://192.168.100.50/tokoonline/public/"
-//    const val baseUrl = "https://09482b35c3b6.ngrok.io/"
+//    const val baseUrl = "http://192.168.100.50/tokoonline/public/"
+    const val baseUrl = "https://aac9-110-137-37-97.ngrok.io/"
     const val produkUrl = baseUrl + "storage/produk/"
 }
